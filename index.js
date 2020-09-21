439a860feef234419fa8fec11355b18253adefba
Hajar Helal <hajerhelal44@gmail.com>
2020-09-21 22:28:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("This is really cool!");
-});
-function DOMContentLoaded() {
-  document.getElementById( "text" )
-    .innerHTML = "JavaScript is so cool. It lets me add text to my page programmatically.";
+
+document.addEventListener( "DOMContentLoaded", function () {
+updateDOM()
+} );
+
+
+function updateDOM() {
+document.getElementById( "text" )
+  .innerHTML = "This is really cool!";
+}nerHTML = "JavaScript is so cool. It lets me add text to my page programmatically.";
 }
