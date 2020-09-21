74a1cca0673f75d90cec5b1ec75fe496f84aeeea
Hajar Helal <hajerhelal44@gmail.com>
2020-09-21 22:26:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("This is really cool!");
 });
+function DOMContentLoaded() {
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
+}
