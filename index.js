a06e887d6c507614ae63300b0c4865cf2065955b
Hajar Helal <hajerhelal44@gmail.com>
2020-09-21 22:28:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,4 @@ updateDOM()
 function updateDOM() {
 document.getElementById( "text" )
   .innerHTML = "This is really cool!";
-}nerHTML = "JavaScript is so cool. It lets me add text to my page programmatically.";
 }
